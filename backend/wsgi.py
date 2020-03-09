0aa9ea73deefe92f68c4eabb747b20bc81a3067b
nutthiyapohn <nutnutnut.1998@gmail.com>
2020-03-09 13:30:39 +0700

    edit gittigore

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -10,7 +10,15 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+# from whitenoise.django import
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
+
+
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
+
+# application = get_wsgi_application()
+# application = DjangoWhiteNoise(application)
